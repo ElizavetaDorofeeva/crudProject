93d9677b0f9ced62149e23d23ff82d8072115230
ElizavetaDorofeeva <Lizaador@mail.ru>
2026-01-11 13:39:26 +0700

    add

--- Tests/BasicTest.cs
+++ Tests/BasicTest.cs
@@ -1,6 +1,13 @@
-﻿namespace WebApplication1.Tests
+﻿using Xunit;
+namespace WebApplication1.Tests
 {
     public class BasicTest
     {
+        [Fact]
+        public void AlwaysPasses()
+        {
+            Assert.True(true);
+        }
     }
 }
+
